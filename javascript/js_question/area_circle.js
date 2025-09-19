d54b65a9d9e69d4fcc15c21c068d12f643353f6f
Megha pant <pantmegha195@gmail.com>
2025-09-19 22:44:14 +0530

    MERN

--- javascript/js_question/area_circle.js
+++ javascript/js_question/area_circle.js
@@ -1,7 +1,6 @@
 // WAP to find the area of a circle?
- 
 
-var pi = 3.14;
-var r = Number(prompt("Enter the radius of circle: "));
-var area = pi * r * r;
-console.log("Area of circle is: " + area);
+var pi = 3.14
+var r = Number(prompt("Enter the radius of circle: "))
+var area = pi * r * r
+console.log("Area of circle is: " + area)
--- javascript/js_question/si.js
+++ javascript/js_question/si.js
@@ -18,3 +18,39 @@
 // console.log("emi intrust is "+emi);
 
 
+let principal = 20000;
+let rate = 10;
+let time = 2;
+
+let si = (principal * rate * time) / 100;
+console.log("Simple Interest:", si);
+
+let total = principal + si;
+console.log("Total Amount to be Paid:", total);
+
+let months = time * 12;
+let emi = total / months;
+console.log("EMI per month:", emi);
+
+
+// WAP to find the area of circle, rectangle, cuboid, square?
+
+var PI = 3.14;
+let r = parseFloat(prompt("Enter radius of circle:"));
+let circleArea = PI * r * r;
+console.log("Area of Circle:", circleArea);
+
+let length = parseFloat(prompt("Enter length of rectangle:"));
+let breadth = parseFloat(prompt("Enter breadth of rectangle:"));
+let rectangleArea = length * breadth;
+console.log("Area of Rectangle:", rectangleArea);
+
+let l = parseFloat(prompt("Enter length of cuboid:"));
+let b = parseFloat(prompt("Enter breadth of cuboid:"));
+let h = parseFloat(prompt("Enter height of cuboid:"));
+let cuboidArea = 2 * (l * b + l * h + b * h);
+console.log("Surface Area of Cuboid:", cuboidArea);
+
+let side = parseFloat(prompt("Enter side of square:"));
+let squareArea = side * side;
+console.log("Area of Square:", squareArea);
